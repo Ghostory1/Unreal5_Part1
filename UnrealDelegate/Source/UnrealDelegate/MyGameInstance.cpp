c2a92eea8e149218c599e07e5f6ebb1aefefcd33
Ghostory <ume6514@gmail.com>
2024-02-06 21:31:10 +0900

    1.9 Delegate

---
+++ UnrealDelegate/Source/UnrealDelegate/MyGameInstance.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+#include "Student.h"
+#include "Teacher.h"
+#include "Staff.h"
+#include "Card.h"
+#include "CourseInfo.h"
+
+UMyGameInstance::UMyGameInstance()
+{
+	SchoolName = TEXT("학교");
+}
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	CourseInfo = NewObject<UCourseInfo>(this);
+
+	UE_LOG(LogTemp, Log, TEXT("========================"));
+
+	UStudent* Student1 = NewObject<UStudent>();
+	Student1->SetName(TEXT("학생1"));
+	UStudent* Student2 = NewObject<UStudent>();
+	Student2->SetName(TEXT("학생2"));
+	UStudent* Student3 = NewObject<UStudent>();
+	Student3->SetName(TEXT("학생3"));
+
+	CourseInfo->OnChanged.AddUObject(Student1, &UStudent::GetNotification);
+	CourseInfo->OnChanged.AddUObject(Student2, &UStudent::GetNotification);
+	CourseInfo->OnChanged.AddUObject(Student3, &UStudent::GetNotification);
+
+	CourseInfo->ChangeCourseInfo(SchoolName, TEXT("변경된 학사 정보"));
+	
+	TArray<UPerson*> Persons = { NewObject<UStudent>(),NewObject<UTeacher>(),NewObject<UStaff>() };
+	for (const auto Person : Persons)
+	{
+		const UCard* OwnCard = Person->GetCard();
+		check(OwnCard);
+		ECardType CardType = OwnCard->GetCardType();
+		UE_LOG(LogTemp, Log, TEXT("%s님이 소유한 카드 종류 %d"), *Person->GetName(), CardType);
+
+		//프로젝트 바꿀때마다 Script안에 이름 바꿔주기
+		const UEnum* CardEnumType= FindObject<UEnum>(nullptr, TEXT("/Script/UnrealDelegate.ECardType"));
+		if (CardEnumType)
+		{
+			FString CardMetaData = CardEnumType->GetDisplayNameTextByValue((int64)CardType).ToString();
+
+			UE_LOG(LogTemp, Log, TEXT("%s님이 소유한 카드 종류 %s"), *Person->GetName(), *CardMetaData);
+		}
+		
+	}
+	UE_LOG(LogTemp, Log, TEXT("========================"));
+}
+
